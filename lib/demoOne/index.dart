1a426fc93fae169bbe3e17338f149e18ce353352
jj <kakaxi@gmail.com>
2022-04-19 17:40:54 +0800

    [add] add canvas demo

--- lib/demoOne/index.dart
+++ lib/demoOne/index.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -9,16 +11,116 @@ class FirstDemo extends StatefulWidget {
 }
 
 class _FirstDemoState extends State<FirstDemo> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("demo one"),),
-      body: Container(
-        width: 375.w,
-        height: 100.w,
-        color: Colors.yellow,
-        child: Text("demo one"),
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 30.w,),
+              Text("画布demo"),
+              SizedBox(height: 30.w,),
+              Container(
+                width: 100.w,
+                height: 100.w,
+                color: Colors.red,
+              ),
+              CustomPaintRoute(),
+            ],
+          ),
+        ),
+      )
+      // body: Expanded(
+      //   child: Center(
+      //     child: Text("page one"),
+      //   ),
+      // ),
+    );
+  }
+}
+
+
+class CustomPaintRoute extends StatelessWidget {
+  const CustomPaintRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RepaintBoundary(
+        child: CustomPaint(
+          size: Size(300, 300),
+          painter: MyPainter(),
+        ),
       ),
     );
   }
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    print("paint");
+    var rect = Offset.zero & size;
+    //画棋盘
+    drawChessBoard(canvas, rect);
+    //画棋子
+    drawPieces(canvas, rect);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+
+  void drawChessBoard(Canvas canvas, Rect rect) {
+    var paint = Paint()
+        ..isAntiAlias = true
+        ..style = PaintingStyle.fill
+        ..color = Colors.yellow;
+    canvas.drawRect(rect, paint);
+
+    paint
+    ..style = PaintingStyle.stroke
+    ..color = Colors.black
+    ..strokeWidth = 1.0;
+
+    for (int i = 0; i <= 15; i++) {
+      double dy = rect.top + rect.height / 15 * i;
+      canvas.drawLine(Offset(rect.left, dy), Offset(rect.right, dy), paint);
+    }
+
+    for (int i = 0; i <= 15; i++) {
+      double dx = rect.left + rect.width / 15 * i;
+      canvas.drawLine(Offset(dx, rect.top), Offset(dx, rect.bottom), paint);
+    }
+  }
+
+  void drawPieces(Canvas canvas, Rect rect) {
+    double eWidth = rect.width / 15;
+    double eHeight = rect.height / 15;
+    //画一个黑子
+    var paint = Paint()
+      ..style = PaintingStyle.fill
+      ..color = Colors.black;
+    //画一个黑子
+    canvas.drawCircle(
+      Offset(rect.center.dx - eWidth / 2, rect.center.dy - eHeight / 2),
+      min(eWidth / 2, eHeight / 2) - 2,
+      paint,
+    );
+    //画一个白子
+    paint.color = Colors.white;
+    canvas.drawCircle(
+      Offset(rect.center.dx + eWidth / 2, rect.center.dy - eHeight / 2),
+      min(eWidth / 2, eHeight / 2) - 2,
+      paint,
+    );
+  }
+
 }
