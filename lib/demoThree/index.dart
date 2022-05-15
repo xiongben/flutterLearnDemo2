2fcf9e924388c8a12eca9b9439e582cc50b88f4f
jj <kakaxi@gmail.com>
2022-05-15 22:01:29 +0800

    [add] column demo

--- lib/demoThree/index.dart
+++ lib/demoThree/index.dart
@@ -44,7 +44,26 @@ class _ThreeDemoState extends State<ThreeDemo> {
                 debugDumpLayerTree();
               },
             ),
-            WrapDemo()
+            WrapDemo(),
+            Container(
+              width: 210.w,
+              color: Colors.blue,
+              child: ResponsiveColumn(
+                children: [
+                  Text("kafajkf"),
+                  Text("kafajkf"),
+                  Text("kafajkf"),
+                  Text("kafajkf"),
+                  Text("kafajkf"),
+                  Text("kafajkf"),
+                  Text("kafajkf"),
+                  Text("kafajkf"),
+                  Text("kafajkf"),
+                  Text("kafajkf"),
+                  Text("kafajkf")
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -131,6 +150,38 @@ class WrapDemo extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class ResponsiveColumn extends StatelessWidget {
+  final List<Widget> children;
+  const ResponsiveColumn({Key? key, required this.children}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth < 200.w) {
+            return Column(
+              children: children,
+              mainAxisSize: MainAxisSize.min,
+            );
+          } else {
+            var _children = <Widget>[];
+            for (var i = 0; i < children.length; i += 2) {
+              if (i+1 < children.length) {
+                _children.add(Row(
+                  children: [children[i], children[i+1]],
+                  mainAxisSize: MainAxisSize.min,
+                ));
+              } else {
+                _children.add(children[i]);
+              }
+            }
+            return Column(children: _children, mainAxisSize: MainAxisSize.min,);
+          }
+        }
     );
   }
 }
