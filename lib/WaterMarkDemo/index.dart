e36c3a4889af06226402540e23aaf95ce57853bf
jj <kakaxi@gmail.com>
2022-05-29 00:05:59 +0800

    [add] waterMark demo

--- lib/WaterMarkDemo/index.dart
+++ lib/WaterMarkDemo/index.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'dart:math' as math;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'dart:ui' as ui;
 
@@ -21,17 +21,41 @@ class _WaterMarkDemoState extends State<WaterMarkDemo> {
           child: Container(
             width: double.infinity,
             padding: EdgeInsets.symmetric(horizontal: 15.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("water mark demo")
-              ],
-            ),
+            child: wTextWaterMark(),
           ),
         )
     );
   }
+
+  Widget wTextWaterMark() {
+    return Stack(
+      children: [
+        Center(
+          child: ElevatedButton(
+            child: const Text('按钮'),
+              onPressed: () => print('tab'),
+            ),
+        ),
+        IgnorePointer(
+          child: Container(
+            child: WaterMark(
+              painter: TextWaterMarkPainter(
+                text: 'Flutter 中国 @wendux',
+                textStyle: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w200,
+                  color: Colors.black38, //为了水印能更清晰一些，颜色深一点
+                ),
+                rotate: -20, // 旋转 -20 度
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
+
 
 class WaterMark extends StatefulWidget {
 
@@ -65,10 +89,31 @@ class _WaterMarkState extends State<WaterMark> {
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+   @override
+   Widget build(BuildContext context) {
+     return SizedBox.expand( // 水印尽可能大
+       child: FutureBuilder(
+         future: _memoryImageFuture,
+         builder: (BuildContext context, AsyncSnapshot snapshot) {
+           if (snapshot.connectionState != ConnectionState.done) {
+             // 如果单元水印还没有绘制好先返回一个空的Container
+             return Container();
+           } else {
+             // 如果单元水印已经绘制好，则渲染水印
+             return DecoratedBox(
+               decoration: BoxDecoration(
+                 image: DecorationImage(
+                   image: snapshot.data, // 背景图，即我们绘制的单元水印图片
+                   repeat: widget.repeat, // 指定重复方式
+                   alignment: Alignment.topLeft,
+                 ),
+               ),
+             );
+           }
+         },
+       ),
+     );
+   }
 
   @override
   void didUpdateWidget(covariant WaterMark oldWidget) {
@@ -87,7 +132,7 @@ class _WaterMarkState extends State<WaterMark> {
     // 创建一个 Canvas 进行离屏绘制，细节和原理请查看本书后面关于Flutter绘制原理相关章节
     final recorder = ui.PictureRecorder();
     final canvas = Canvas(recorder);
-    final Size size = widget.painter.paintUnit(canvas);
+    final Size size = widget.painter.paintUnit(canvas, 2.75);
     final picture = recorder.endRecording();
     //将单元水印导为图片并缓存起来
     final img = await picture.toImage(size.width.ceil(), size.height.ceil());
@@ -99,8 +144,8 @@ class _WaterMarkState extends State<WaterMark> {
 
 
 abstract class WaterMarkPainter {
-  // Size paintUnit(Canvas canvas, double devicePixelRatio);
-  Size paintUnit(Canvas canvas);
+  Size paintUnit(Canvas canvas, double devicePixelRatio);
+  // Size paintUnit(Canvas canvas);
   bool shouldRepaint(covariant WaterMarkPainter oldPainter) => true;
 }
 
@@ -138,14 +183,105 @@ class TextWaterMarkPainter extends WaterMarkPainter {
   // 4,调用 canvas.drawParagraph 绘制。
 
   @override
-  Size paintUnit(Canvas canvas) {
-    // TODO: implement paintUnit
-    throw UnimplementedError();
+  Size paintUnit(Canvas canvas, double devicePixelRatio) {
+    //根据屏幕 devicePixelRatio 对文本样式中长度相关的一些值乘以devicePixelRatio
+    final _textStyle = _handleTextStyle(textStyle, devicePixelRatio);
+    final _padding = padding * devicePixelRatio;
+
+    // //构建文本段落
+    // final builder = ui.ParagraphBuilder(_textStyle.getParagraphStyle(
+    //   textDirection: TextDirection.ltr,
+    //   textAlign: TextAlign.center,
+    //   textScaleFactor: devicePixelRatio
+    // ));
+    //
+    // //添加要绘制的文本及样式
+    // builder
+    //   ..pushStyle(_textStyle.getTextStyle()) // textStyle 为 ui.TextStyle
+    //   ..addText(text);
+    //
+    // //layout 后我们才能知道文本占用的空间
+    // ui.Paragraph paragraph = builder.build()
+    //   ..layout(ui.ParagraphConstraints(width: double.infinity));
+    //
+    // //文本占用的真实宽度
+    // final textWidth = paragraph.longestLine.ceilToDouble();
+    // //文本占用的真实高度
+    // final fontSize = paragraph.height;
+    //
+    // //绘制文本
+    // canvas.drawParagraph(paragraph, Offset.zero);
+
+
+    //构建文本画笔
+    TextPainter painter = TextPainter(
+      textDirection: TextDirection.ltr,
+      textScaleFactor: devicePixelRatio,
+    );
+    //添加文本和样式
+    painter.text = TextSpan(text: text, style: _textStyle);
+    //对文本进行布局
+    painter.layout();
+
+    //文本占用的真实宽度
+    final textWidth = painter.width;
+    //文本占用的真实高度
+    final fontSize = painter.height;
+
+    // 将弧度转化为度数
+    final radians = math.pi * rotate / 180;
+
+    //通过三角函数计算旋转后的位置和size
+    final orgSin = math.sin(radians);
+    final sin = orgSin.abs();
+    final cos = math.cos(radians).abs();
+
+    final width = textWidth * cos;
+    final height = textWidth * sin;
+    final adjustWidth = fontSize * sin;
+    final adjustHeight = fontSize * cos;
+
+    // 为什么要平移？下面解释
+    if (orgSin >= 0) { // 旋转角度为正
+      canvas.translate(
+        adjustWidth + padding.left,
+        padding.top,
+      );
+    } else { // 旋转角度为负
+      canvas.translate(
+        padding.left,
+        height + padding.top,
+      );
+    }
+    canvas.rotate(radians);
+
+    // 绘制文本
+    painter.paint(canvas, Offset.zero);
+
+    // 返回水印单元所占的真实空间大小（需要加上padding）
+    return Size(
+      width + adjustWidth + padding.horizontal,
+      height + adjustHeight + padding.vertical,
+    );
+
   }
 
   @override
-  bool shouldRepaint(covariant WaterMarkPainter oldPainter) {
-    // TODO: implement shouldRepaint
-    return super.shouldRepaint(oldPainter);
+  bool shouldRepaint(TextWaterMarkPainter oldPainter) {
+    return oldPainter.rotate != rotate ||
+        oldPainter.text != text ||
+        oldPainter.padding != padding ||
+        oldPainter.textStyle != textStyle;
+  }
+
+  TextStyle _handleTextStyle(TextStyle textStyle,double devicePixelRatio) {
+    var style = textStyle;
+    double _scale(attr) => attr == null ? 1.0 : devicePixelRatio;
+    return style.apply(
+      decorationThicknessFactor: _scale(style.decorationThickness),
+      letterSpacingFactor: _scale(style.letterSpacing),
+      wordSpacingFactor: _scale(style.wordSpacing),
+      heightFactor: _scale(style.height)
+    );
   }
 }
